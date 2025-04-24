d211530791c16b93b96754eeff79e1609e2a0c53
Manuela Berrio <berrio@usc.edu>
2025-04-24 07:23:42 +0000

    done1

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,14 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(
+  const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shiftCnt,
+    size_t day
+    );
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -37,9 +45,42 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
+    size_t n= avail.size();
+    size_t k =avail[0].size();
+    sched.assign(n, vector<Worker_T>());
+    vector<size_t> shiftCnt(k,0);
+    return scheduleHelper(avail,dailyNeed,maxShifts,sched,shiftCnt,0);
 
 }
 
+bool scheduleHelper(
+  const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shiftCnt,
+    size_t day
+    ){
+      if(day==avail.size()){
+        return true;
+      }
+      size_t k = avail[0].size();
+
+      if(sched[day].size() == dailyNeed){
+        return scheduleHelper(avail, dailyNeed,maxShifts,sched,shiftCnt,day+1);        
+      }
+      for(size_t w =0; w<k; ++w){
+        if(shiftCnt[w]<maxShifts && find(sched[day].begin(),sched[day].end(),w)==sched[day].end() && avail[day][w]){
+          sched[day].push_back(w);
+          shiftCnt[w]++;
+
+          if(scheduleHelper(avail,dailyNeed,maxShifts,sched,shiftCnt,day)){
+            return true;
+          }
+
+          sched[day].pop_back();
+          shiftCnt[w]--;
+        }
+      }
+      return false;
+    }
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <string>
 #endif
 
 #include "wordle.h"
@@ -13,6 +14,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+int countLetters(const string& str, char c);
+void wordleHelper(string curr, string floating, const set<string>& dict, set<string>& results, unsigned int idx);
 
 
 // Definition of primary wordle function
@@ -22,7 +25,55 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set <string> results;
+    wordleHelper(in, floating, dict, results, 0);
+    return results;
 
 }
 
 // Define any helper functions here
+int countLetters(const string& str, char c){
+  int cnt = 0;
+  for(unsigned int i =0; i<str.size(); ++i){
+    if(str[i] == c){
+      cnt++;
+    }
+  }
+  return cnt;
+
+}
+void wordleHelper(string curr, string floating, const set<string>& dict, set<string>& results, unsigned int idx){
+  if(idx == curr.size()){
+    if(dict.count(curr) > 0 && floating.empty()){
+      results.insert(curr);
+    }
+    return;
+  }
+
+  if(curr[idx] !='-'){
+    wordleHelper(curr, floating, dict, results, idx+1);
+    return;
+  }
+
+  for(char c = 'a'; c<='z'; ++c){
+    curr[idx] = c;
+    bool isFloating = false;
+    for(unsigned int i = 0; i<floating.size();++i){
+      if(floating[i]==c){
+        string update = floating;
+        update.erase(i,1);
+        wordleHelper(curr, update, dict, results, idx+1);
+        isFloating=true;
+        break;
+      }
+    }
+    if(!isFloating){
+      int blanksLeft = countLetters(curr.substr(idx),'-');
+      if(blanksLeft >=(int)floating.size()){
+        wordleHelper(curr, floating, dict, results, idx+1);
+      }
+    }
+  }
+}
+
+
